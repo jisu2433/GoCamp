07df26cab7051744bbd994ba89f389920570f79a
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-05-16 23:54:30 +0900

    Add : New

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.render('home')
 })
@@ -22,8 +24,18 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
-  const campground = await Campground.findById(req.params)
+  const campground = await Campground.findById(req.params.id)
   res.render('campground/show', { campground });
 })
 
