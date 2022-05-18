9917e369494f93fc41d62a7f1be3de5873745edb
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-05-18 23:38:56 +0900

    Add : 스타일링

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
@@ -11,6 +12,7 @@ db.on("error", console.error.bind(console, "connection error:"));
 
 const app = express();
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
