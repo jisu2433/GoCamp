39ca42f93c500941844f84609d530187c1c01855
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-05-23 21:58:47 +0900

    Add : 리뷰 모델 추가하기

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const Joi = require('joi');
-const { campgroundSchema } = require('./schemas.js');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError')
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const http = require('http');
 const cors = require('cors');
-
+const Review = require('./models/review');
 
 const mongoDB = 'mongodb://localhost:27017/go-camp';
 mongoose.connect(mongoDB);
@@ -30,6 +30,16 @@ app.use(methodOverride('_method'))
 
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else {
+    next();
+  }
+}
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
   if (error) {
     const msg = error.details.map(el => el.message).join(',')
     throw new ExpressError(msg, 400)
@@ -60,7 +70,8 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 }))
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-  const campground = await Campground.findById(req.params.id)
+  const campground = await Campground.findById(req.params.id).populate('reviews');
+  console.log(campground);
   res.render('campgrounds/show', { campground });
 }));
 
@@ -80,6 +91,22 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds');
 }));
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId }})
+  await Review.findByIdAndDelete(req.params.reviewId);
+  res.redirect(`/campgrounds/${id}`)
+}))
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404))
